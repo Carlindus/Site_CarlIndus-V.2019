926f105df7b2c535049b3e8cccdd9e5e370659c1
carlindus <carlindusdesign@gmail.com>
2019-10-03 11:06:21 +0200

    reorganize icon files -> normalize naming & extensions ; update config_content

---
+++ cid/config/config_site.php
@@ -0,0 +1,22 @@
+<?php
+// Path configuration
+$RELATIVE_PATH = 'cid';
+$ASSETS_PATH = $RELATIVE_PATH.'/assets';
+$PHP_PATH = $RELATIVE_PATH.'/phpPart';
+$DOCUMENTS_PATH = '';
+$DOWNLOAD_PATH = $RELATIVE_PATH.'/downloads';
+
+// Content of the site
+$content_url = "$RELATIVE_PATH/config/config_content.json";
+$CONTENT = json_decode(file_get_contents($content_url));
+
+// Meta config of the site
+$lang = "fr"; 
+$site_title="CarlIndus Design";
+$site_description="description de la page";
+
+// Recaptcha configuration
+$RECAPTCHA_KEY = "XLdeDKcUAAAAAPR_wsJA4i_lmzMpJnkjgEKzbN44"; // Fake value
+$RECAPTCHA_SECRET = "XLdeDKcUAAAAAPkPqW54gIJHj2aMcJ_3d8iSr6wi"; // Fake value
+
+?>
